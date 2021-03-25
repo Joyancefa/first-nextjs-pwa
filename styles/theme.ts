582b14b94edc73a80ca3f13f0ccdf6c87ec1dac4
Joyancefa <joyancefa@gmail.com>
2021-03-25 09:37:03 +0100

    use ts extension for theme file

--- styles/theme.ts
+++ styles/theme.ts
@@ -4,14 +4,14 @@ import { extendTheme } from '@chakra-ui/react';
 // This will apply a `backgroundColor`, font `color` to the html, body elements
 // Colors equivalent can be found here => https://chakra-ui.com/docs/theming/theme
 const styles = {
-    global: {
-      "html, body": {
-        color: "white",
-        backgroundColor: "blue.900",
-      },
+  global: {
+    'html, body': {
+      color: 'white',
+      backgroundColor: 'blue.900',
     },
-  };
+  },
+};
 
 // Use the style in the theme and return it
-const theme = extendTheme({styles});
+const theme = extendTheme({ styles });
 export default theme;
