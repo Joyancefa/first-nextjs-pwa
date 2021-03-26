06d60099073dfc10669a3f8c7d7f6d1a7c73c077
Joyancefa <joyancefa@gmail.com>
2021-03-26 07:46:26 +0100

    fix

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,14 +2,13 @@ module.exports = {
     extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
     plugins: ['stylelint-order'],
     rules: {
-        'order/properties-alphabetical-order': true,
+        'order/properties-alphabetical-order': true, // Ensure CSS properties are ordered
         'at-rule-no-unknown': [
         true,
         {
-            ignoreAtRules: ['tailwind', 'apply', 'variants', 'responsive', 'screen', 'layer'],
+            ignoreAtRules: ['tailwind', 'apply', 'variants', 'responsive', 'screen', 'layer'], // Stylelint won't flag if it finds any of these strings
         },
         ],
-        'declaration-block-trailing-semicolon': null,
-        'no-descending-specificity': null,
+        'declaration-block-trailing-semicolon': "always", // Ensure there is a semi-colon at the end of a declaration block
     },
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,8 @@ import { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from '../styles/theme';
 
-function MyApp({ Component, pageProps }: AppProps) {return (
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
